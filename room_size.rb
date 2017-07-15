21c46665e3df063d3efb2d5d2dceafdb263b40e3
Kelly Netterville <bitgrease@gmail.com>
2017-07-14 23:45:22 -0400

    Room size problem.

--- room_size.rb
+++ room_size.rb
@@ -2,14 +2,14 @@
 # Output - area of the room in square feet and square meters.
 # Additional - Do not worry about validating Input
 # 1 sqm = 10.7639 sqft
-
+SQMETERS_TO_FEET = 10.7639
 def room_size
   print 'Enter the length of the room in meters: '
   length_in_meters = gets.chomp
   print 'Enter the width of the room in meters: '
   width_in_meters = gets.chomp
   area_in_meters = width_in_meters.to_f * length_in_meters.to_f
-  area_in_feet = area_in_meters * 10.7639
+  area_in_feet = area_in_meters * SQMETERS_TO_FEET
   puts "The area of the room is #{format('%.1f', area_in_meters)} square " \
        + "meters (#{format('%.2f', area_in_feet)} square feet)."
 end
