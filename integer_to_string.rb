d0c1539ce1098408bfa38609cc27d074a8fca7ca
Kelly Netterville <bitgrease@gmail.com>
2017-07-26 22:46:48 -0400

    Integer to string exercise.

---
+++ integer_to_string.rb
@@ -0,0 +1,10 @@
+def integer_to_string(integer)
+  digits = %w[0 1 2 3 4 5 6 7 8 9]
+  number = ''
+  loop do 
+    integer, remainder = integer.divmod(10)
+    number.prepend(digits[remainder])
+    break if integer == 0
+  end
+  number
+end
