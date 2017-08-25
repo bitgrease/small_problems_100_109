1d3303d48cd8c5cad7f5f0638ea0de4bbd0ca2fa
Kelly Netterville <bitgrease@gmail.com>
2017-08-25 18:40:45 -0400

    minilang with further exploration including checks for stack empty and invalid commands.

--- mini_lang.rb
+++ mini_lang.rb
@@ -1,56 +1,57 @@
+def push(stack, register)
+  stack << register
+end
+
+def add(stack, register)
+  abort "Error - Stack Empty!" if stack.empty?
+  register += stack.pop
+end
+
+def sub(stack, register)
+  abort "Error - Stack Empty!" if stack.empty?
+  register -= stack.pop
+end
+
+def mult(stack, register)
+  abort "Error - Stack Empty!" if stack.empty?
+  register *= stack.pop
+end
+
+def div(stack, register)
+  abort "Error - Stack Empty!" if stack.empty?
+  register /= stack.pop
+end
+
+def mod(stack, register)
+  abort "Error - Stack Empty!" if stack.empty?
+  register %= stack.pop
+end
+
+def pop(stack, register)
+  abort "Error - Stack Empty!" if stack.empty?
+  register = stack.pop
+end
+
 def minilang(str)
   memory = { register: 0, stack: [] }
   inputs = str.split
-  
   until inputs.empty?
     input = inputs.shift
-    case input
+    case input.upcase
     when /[0-9]+/ then memory[:register] = input.to_i
-    when 'PUSH' then memory[:stack] << memory[:register]
-    when 'ADD' then memory[:register] += memory[:stack].pop
-    when 'SUB' then memory[:register] -= memory[:stack].pop
-    when 'MULT' then memory[:register] *= memory[:stack].pop
-    when 'DIV' then memory[:register] /= memory[:stack].pop
-    when 'MOD' then memory[:register] %= memory[:stack].pop
-    when 'POP' then memory[:register] = memory[:stack].pop
+    when 'PUSH' then push(memory[:stack], memory[:register])
+    when 'ADD' then memory[:register] = add(memory[:stack], memory[:register])
+    when 'SUB' then memory[:register] = sub(memory[:stack], memory[:register])
+    when 'MULT' then memory[:register] = mult(memory[:stack], memory[:register])
+    when 'DIV' then memory[:register] = div(memory[:stack], memory[:register])
+    when 'MOD' then memory[:register] = mod(memory[:stack], memory[:register])
+    when 'POP' then memory[:register] = pop(memory[:stack], memory[:register])
     when 'PRINT' then puts memory[:register]
-
+    else 
+      abort('Aborted! Invalid Parameter!')
     end
-  end  
-  memory
+  end
+  memory[:register]
 end
 
-minilang('PRINT')
-# 0
-
-minilang('5 PUSH 3 MULT PRINT')
-# 15
-
-minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
-# 5
-# 3
-# 8
-
-minilang('5 PUSH POP PRINT')
-# 5
-
-minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
-# 5
-# 10
-# 4
-# 7
-
-minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
-# 6
-
-minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
-# 12
-
-minilang('-3 PUSH 5 SUB PRINT')
-# 8
-
-minilang('6 PUSH')
-
-# Further Exploration
-
-minilang('4')
+p minilang("3 PUSH 5 MOD PUSH #{minilang('-7 PUSH 3 PUSH 4 PUSH 5 MULT ADD add')} DIV")
