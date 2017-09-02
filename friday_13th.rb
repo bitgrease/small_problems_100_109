ba1cf2e1af19e5d9066b905fa39291391644993d
Kelly Netterville <bitgrease@gmail.com>
2017-09-01 21:13:36 -0400

    Update to exercise

--- friday_13th.rb
+++ friday_13th.rb
@@ -1,3 +1,19 @@
+require 'date'
+# if year starts on Sunday (3 friday 13ths)
+# if month starts with Sunday, will have a friday 13th
+
 def friday_13th(year)
 
+  first_of_month = Date.new(year.to_i, 1, 1)
+
+  # If January starts on Sunday, the year will have 3 Friday 13ths
+  return 3 if first_of_month.sunday?
+
+  bad_fridays = 0
+  
+  11.times do 
+    first_of_month = first_of_month.next_month
+    bad_fridays += 1 if first_of_month.sunday?
+  end
+  bad_fridays
 end
